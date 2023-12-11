1e48bfdaa2e6b57b6f6b34596fad4f96557ecdc4
Sören Reichardt <soren.reichardt@neo4j.com>
2023-12-11 14:34:57 +0100

    Solve day10 part2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -4,3 +4,8 @@ pub fn part1() {
     let input = std::str::from_utf8(include_bytes!("../../resource/day10/input1")).unwrap();
     println!("{}", pipe_maze::farthest_point_in_loop(input))
 }
+
+pub fn part2() {
+    let input = std::str::from_utf8(include_bytes!("../../resource/day10/input1")).unwrap();
+    println!("{}", pipe_maze::enclosed_tiles(input))
+}
--- src/day10/pipe_maze.rs
+++ src/day10/pipe_maze.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fmt::{Display, Formatter, Write};
 use std::str::FromStr;
 
@@ -11,7 +11,7 @@ struct Maze {
     start_position: (usize, usize),
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 enum Tile {
     Air,
     Start,
@@ -26,6 +26,12 @@ enum Tile {
 pub fn farthest_point_in_loop(input: &str) -> u32 {
     let maze = parse_maze(input);
     maze.farthest_distance_in_loop()
+}
+
+pub fn enclosed_tiles(input: &str) -> u32 {
+    let maze = parse_maze(input);
+    let main_loop = maze.calculate_main_loop();
+    maze.enclosed_tiles(main_loop).len() as u32
 }
 
 fn parse_maze(input: &str) -> Maze {
@@ -188,7 +194,17 @@ impl Maze {
         }
 
         while let Some((next_position, from, distance_from_start)) = traverse_queue.pop() {
-            self.traverse(next_position, from, &mut distances, distance_from_start, &mut traverse_queue);
+            self.traverse(next_position, from, distance_from_start, &mut traverse_queue, |(row, col), dst, _| match distances.get_mut(&(row, col)) {
+                Some(distance) if distance_from_start < *distance => {
+                    *distance = distance_from_start;
+                    true
+                },
+                Some(_) => false,
+                None => {
+                    distances.insert((row, col), distance_from_start);
+                    true
+                }
+            });
         }
 
         let mut distance_values = distances.values().collect::<Vec<_>>();
@@ -196,13 +212,12 @@ impl Maze {
         **distance_values.last().unwrap()
     }
 
-    fn traverse(&self, (row, col): (usize, usize), from: usize, distances: &mut HashMap<(usize, usize), u32>, distance_from_start: u32, queue: &mut Vec<((usize, usize), usize, u32)>) {
+    fn traverse<F: FnMut((usize, usize), u32, usize) -> bool>(&self, (row, col): (usize, usize), from: usize, distance_from_start: u32, queue: &mut Vec<((usize, usize), usize, u32)>, mut traverse_fn: F) {
         let distance_from_start = distance_from_start + 1;
-        match distances.get_mut(&(row, col)) {
-            Some(distance) if distance_from_start < *distance => *distance = distance_from_start,
-            Some(_) => return,
-            None => { distances.insert((row, col), distance_from_start); }
-        };
+        if !traverse_fn((row, col), distance_from_start, from) {
+            return
+        }
+
         let (next_position, from) = match (&self.tiles[row][col], from) {
             (Tile::Horizontal, EAST) => ((row, col - 1), EAST),
             (Tile::Horizontal, WEST) => ((row, col + 1), WEST),
@@ -221,11 +236,157 @@ impl Maze {
 
         queue.push((next_position, from, distance_from_start));
     }
+
+    fn calculate_main_loop(&self) -> HashSet<(usize, usize)> {
+        let (start_row, start_col) = self.start_position;
+        let start_tile = &self.tiles[start_row][start_col];
+        let mut traverse_queue = Vec::new();
+
+        match start_tile {
+            Tile::Vertical => traverse_queue.push(((start_row - 1, start_col), SOUTH, 0)),
+            Tile::Horizontal => traverse_queue.push(((start_row, start_col - 1), EAST, 0)),
+            Tile::NorthEast => traverse_queue.push(((start_row - 1, start_col), SOUTH, 0)),
+            Tile::NorthWest => traverse_queue.push(((start_row - 1, start_col), SOUTH, 0)),
+            Tile::SouthEast => traverse_queue.push(((start_row + 1, start_col), NORTH, 0)),
+            Tile::SouthWest => traverse_queue.push(((start_row + 1, start_col), NORTH, 0)),
+            _ => panic!("Invalid start tile {start_tile}")
+        };
+
+        let mut main_loop = HashSet::new();
+
+        while let Some((next_position, from, distance_from_start)) = traverse_queue.pop() {
+            self.traverse(next_position, from, distance_from_start, &mut traverse_queue, |pos, _, _| main_loop.insert(pos));
+        }
+
+        main_loop
+    }
+
+    fn enclosed_tiles(&self, main_loop: HashSet<(usize, usize)>) -> HashSet<(usize, usize)> {
+        let (start_row, start_col) = self.start_position;
+        let start_tile = &self.tiles[start_row][start_col];
+        let mut traverse_queue = Vec::new();
+
+        match start_tile {
+            Tile::Vertical => traverse_queue.push(((start_row - 1, start_col), SOUTH, 0)),
+            Tile::Horizontal => traverse_queue.push(((start_row, start_col - 1), EAST, 0)),
+            Tile::NorthEast => traverse_queue.push(((start_row - 1, start_col), SOUTH, 0)),
+            Tile::NorthWest => traverse_queue.push(((start_row - 1, start_col), SOUTH, 0)),
+            Tile::SouthEast => traverse_queue.push(((start_row + 1, start_col), NORTH, 0)),
+            Tile::SouthWest => traverse_queue.push(((start_row + 1, start_col), NORTH, 0)),
+            _ => panic!("Invalid start tile {start_tile}")
+        };
+
+        let mut enclosed = HashSet::new();
+        while let Some((next_position, from, distance_from_start)) = traverse_queue.pop() {
+            self.traverse(next_position, from, distance_from_start, &mut traverse_queue, |pos, _, from| {
+                if pos == self.start_position {
+                    return false;
+                }
+                if (from == SOUTH && [Tile::Vertical, Tile::SouthWest].contains(&self.tiles[pos.0][pos.1]))
+                    || (from == WEST && &self.tiles[pos.0][pos.1] == &Tile::NorthWest) {
+                    let mut col = pos.1 + 1;
+                    loop {
+                        if main_loop.contains(&(pos.0, col)) {
+                            return true;
+                        }
+                        if col == self.width {
+                            enclosed.clear();
+                            return false;
+                        }
+
+                        enclosed.insert((pos.0, col));
+                        col += 1;
+                    }
+                }
+                true
+            });
+        }
+
+        if enclosed.len() != 0 {
+            return enclosed;
+        }
+
+        let mut traverse_queue = Vec::new();
+        match start_tile {
+            Tile::Vertical => traverse_queue.push(((start_row - 1, start_col), SOUTH, 0)),
+            Tile::Horizontal => traverse_queue.push(((start_row, start_col - 1), EAST, 0)),
+            Tile::NorthEast => traverse_queue.push(((start_row - 1, start_col), SOUTH, 0)),
+            Tile::NorthWest => traverse_queue.push(((start_row - 1, start_col), SOUTH, 0)),
+            Tile::SouthEast => traverse_queue.push(((start_row + 1, start_col), NORTH, 0)),
+            Tile::SouthWest => traverse_queue.push(((start_row + 1, start_col), NORTH, 0)),
+            _ => panic!("Invalid start tile {start_tile}")
+        };
+
+        while let Some((next_position, from, distance_from_start)) = traverse_queue.pop() {
+            self.traverse(next_position, from, distance_from_start, &mut traverse_queue, |pos, _, from| {
+                if pos == self.start_position {
+                    return false;
+                }
+                if (from == SOUTH && [Tile::Vertical, Tile::SouthEast].contains(&self.tiles[pos.0][pos.1]))
+                    || (from == EAST && &self.tiles[pos.0][pos.1] == &Tile::NorthEast) {
+                    let mut col = pos.1 - 1;
+                    loop {
+                        if main_loop.contains(&(pos.0, col)) {
+                            return true;
+                        }
+                        if col == 0 {
+                            enclosed.clear();
+                            return false;
+                        }
+
+                        enclosed.insert((pos.0, col));
+                        col -= 1;
+                    }
+                }
+                true
+            });
+        }
+
+        enclosed
+    }
+}
+
+impl Tile {
+    fn connects_horizontally_to_previous(&self, other: &Tile) -> bool {
+        match self {
+            Tile::Horizontal | Tile::NorthWest | Tile::SouthWest if [Tile::NorthEast, Tile::SouthEast, Tile::Horizontal].contains(&other) => true,
+            _ => false
+        }
+    }
+
+    fn connects_vertically_to_previous(&self, other: &Tile) -> bool {
+        match self {
+            Tile::Vertical | Tile::NorthEast | Tile::NorthWest if [Tile::Vertical, Tile::SouthWest, Tile::SouthEast].contains(&other) => true,
+            _ => false
+        }
+    }
+}
+
+#[derive(Debug)]
+enum SweepState {
+    Open,
+    Closed
+}
+
+impl SweepState {
+    fn toggle(&mut self) -> SweepState {
+        match self {
+            SweepState::Open => SweepState::Closed,
+            SweepState::Closed => SweepState::Open
+        }
+    }
+
+    fn is_open(&self) -> bool {
+        match self {
+            SweepState::Open => true,
+            SweepState::Closed => false
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day10::pipe_maze::{parse_maze, Tile};
+    use crate::day10::pipe_maze::{enclosed_tiles, parse_maze, Tile};
 
     #[test]
     fn should_parse_maze() {
@@ -273,6 +434,53 @@ J||"#;
         let maze = parse_maze(input);
         let (start_row, start_col) = maze.start_position;
         assert_eq!(maze.tiles[start_row][start_col], Tile::Vertical)
+    }
+
+    #[test]
+    fn should_count_enclosed_tiles() {
+        let input = r#"..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+.........."#;
+
+        assert_eq!(enclosed_tiles(input), 4);
+    }
+
+    #[test]
+    fn should_count_complex_enclosed_tiles() {
+        let input = r#".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ..."#;
+
+        assert_eq!(enclosed_tiles(input), 8);
+    }
+
+    #[test]
+    fn should_count_complex_enclosed_tiles_2() {
+        let input = r#"FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L"#;
+
+        assert_eq!(enclosed_tiles(input), 10);
     }
 }
 

e154582a4cee80d3994b8c76e13684f8747424a7
Sören Reichardt <soren.reichardt@neo4j.com>
2023-12-15 09:10:16 +0100

    Solve day13 part2

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -4,8 +4,3 @@ pub fn part1() {
     let input = std::str::from_utf8(include_bytes!("../../resource/day13/input1")).unwrap();
     println!("{}", point_of_incidence::reflecting_notes_count(input))
 }
-
-pub fn part2() {
-    let input = std::str::from_utf8(include_bytes!("../../resource/day13/input1")).unwrap();
-    // println!("{}", crate::day12::hot_springs::count_arrangements(input, 5))
-}
--- src/day13/point_of_incidence.rs
+++ src/day13/point_of_incidence.rs
@@ -15,26 +15,15 @@ pub fn reflecting_notes_count(input: &str) -> usize {
 fn find_reflection_in_pattern(pattern: Vec<Vec<char>>) -> usize {
     // vertical reflections
     let vertical_match = pattern[0].windows(2).enumerate()
-        .filter_map(|(start, window)| {
-            if window[0] == window[1] {
-                return Some(start);
-            }
-            None
-        })
+        .map(|(start, _)| start)
         .filter(|candidate| check_vertical_reflection_candidate(&pattern, *candidate))
         .next();
 
     let horizontal_match = pattern.windows(2).enumerate()
-        .filter_map(|(start, window)| {
-            if window[0] == window[1] {
-                return Some(start);
-            }
-            None
-        })
+        .map(|(start, _)| start)
         .filter(|candidate| check_horizontal_reflection_candidate(&pattern, *candidate))
         .next();
 
-    println!("{:?} {:?}", vertical_match, horizontal_match);
     match (vertical_match, horizontal_match) {
         (Some(vertical), None) => vertical + 1,
         (None, Some(horizontal)) => 100 * (horizontal + 1),
@@ -45,19 +34,29 @@ fn find_reflection_in_pattern(pattern: Vec<Vec<char>>) -> usize {
 fn check_horizontal_reflection_candidate(pattern: &Vec<Vec<char>>, start: usize) -> bool {
     let start = start + 1;
     let check_length = usize::min(pattern.len() - start, start);
-    (start..(start + check_length)).all(|row_num| {
-        pattern[row_num] == pattern[(start - 1) - (row_num - start)]
-    })
+    (start..(start + check_length)).fold(0, |wrong_matches, row_num| {
+        wrong_matches + (0..pattern[0].len()).fold(0, |wrong_matches, col_num| {
+            if pattern[row_num][col_num] == pattern[(start - 1) - (row_num - start)][col_num] {
+                wrong_matches
+            } else {
+                wrong_matches + 1
+            }
+        })
+    }) == 1
 }
 
 fn check_vertical_reflection_candidate(pattern: &Vec<Vec<char>>, start: usize) -> bool {
     let start = start + 1;
     let check_length = usize::min(pattern[0].len() - start, start);
-    (start..(start + check_length)).all(|col_num| {
-        pattern.iter().all(|row| {
-            row[col_num] == row[(start - 1) - (col_num - start)]
+    (start..(start + check_length)).fold(0, |wrong_matches, col_num| {
+        wrong_matches + pattern.iter().fold(0, |wrong_matches, row| {
+            if row[col_num] == row[(start - 1) - (col_num - start)] {
+                wrong_matches
+            } else {
+                wrong_matches + 1
+            }
         })
-    })
+    }) == 1
 }
 
 #[cfg(test)]
@@ -73,7 +72,7 @@ mod tests {
 ..#.##.#.
 ..##..##.
 #.#.##.#."#;
-        assert_eq!(reflecting_notes_count(input), 5);
+        assert_eq!(reflecting_notes_count(input), 300);
     }
 
     #[test]
@@ -85,6 +84,6 @@ mod tests {
 #####.##.
 ..##..###
 #....#..#"#;
-        assert_eq!(reflecting_notes_count(input), 400);
+        assert_eq!(reflecting_notes_count(input), 100);
     }
 }
